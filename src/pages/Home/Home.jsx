b41940b819a913be716ebdc95b7419230d56af13
ChrisLCoop <christian.limas.e@gmail.com>
2024-01-22 23:49:07 -0500

    45456asdasd

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,9 @@
-import Carru from "../../Carru";
-//import Categoria from "../../cristhian/Categoria";
-import Novedades from "../../cristhian/Novedades";
-import Slider from "../../cristhian/Slider";
-import SliderCesar from "../../cesar/SliderCesar";
-import '../../cesar/Categorias.css'
-import '../../cesar/ModoDarck1.css'
-import '../../styles/Slider.css'
-//import Galeria from "../../cristhian/Galeria";
-//import Especificaciones from "../../cristhian/Especificaciones";
+import React from 'react'
 
-
-const Home = ({filteredApiNew}) => {
+const Home = () => {
   return (
-    <>
-    
-      <Carru />
-      <Novedades items={filteredApiNew} />
-      <SliderCesar />
-      <Slider />
-     
-     
-      
-    
-    </>
-  );
-};
+    <div>Home</div>
+  )
+}
 
-export default Home;
+export default Home
